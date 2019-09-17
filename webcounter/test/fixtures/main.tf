fb3f85f18823df86ca86291a6205d39b6913f72a
Slav Donchev <slav.donchev@gmail.com>
2019-09-17 12:29:16 +0300

    wc test use latest ami

--- webcounter/test/fixtures/main.tf
+++ webcounter/test/fixtures/main.tf
@@ -30,9 +30,15 @@ data "aws_subnet_ids" "default_subnets" {
   vpc_id = tolist(data.aws_vpcs.default.ids)[0]
 }
 
+data "aws_ami" "wc_latest" {
+  owners           = ["self"]
+  most_recent      = true
+  name_regex       = "^webcounter-\\d+\\.\\d+\\.\\d+-\\d+$"
+}
+
 module "webcounter" {
   source           = "../../"
-  ami_id           = var.ami_id
+  ami_id           = data.aws_ami.wc_latest.id
   key_name         = aws_key_pair.kitchen-test.key_name
   private_key_path = "${path.root}/${var.ssh_private_key_path}"
   redis_address    = var.redis_address
