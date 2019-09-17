09c3456b491c0637ac709b9dc002c6f6c1885231
Slav Donchev <slav.donchev@gmail.com>
2019-09-17 09:31:46 +0300

    set wc ec2 to use userdata

--- webcounter/ec2-instance.tf
+++ webcounter/ec2-instance.tf
@@ -6,42 +6,18 @@ resource "aws_instance" "webcounter" {
   vpc_security_group_ids      = [aws_security_group.sg_default_webcounter.id]
   associate_public_ip_address = var.associate_public_ip_address
   key_name                    = var.key_name
+  user_data                   = <<-EOT
+              #!/usr/bin/env bash
+              mkdir -p /etc/systemd/system/webcounter.service.d/
+              cat <<'EOF' > /etc/systemd/system/webcounter.service.d/environment.conf
+              [Service]
+              Environment='REDIS_ADDR=${var.redis_address}' 'REDIS_PASS=${var.redis_password}'
+
+              EOF
+              sudo systemctl daemon-reload
+              systemctl restart webcounter.service
+              EOT
   lifecycle {
     create_before_destroy = true
-  }
-}
-
-resource "null_resource" "provision" {
-
-  count = var.instance_count
-
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    host        = aws_instance.webcounter[count.index].public_dns
-    private_key = file(var.private_key_path)
-  }
-
-  provisioner "file" {
-    content = <<-EOF
-                  [Service]
-                  Environment='REDIS_ADDR=${var.redis_address}' 'REDIS_PASS=${var.redis_password}'
-                  EOF
-    destination = "/tmp/environment.conf"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo mkdir -p /etc/systemd/system/webcounter.service.d/",
-      "sudo cp /tmp/environment.conf /etc/systemd/system/webcounter.service.d/.",
-      "sudo systemctl daemon-reload",
-      "sudo systemctl restart webcounter.service",
-      "rm /tmp/environment.conf",
-    ]
-  }
-
-  triggers = {
-    redis_addr = var.redis_address
-    redis_pass = var.redis_password
   }
 }
