319cdf98f48e35b5ca0465351a56bcdba30f1549
Slav Donchev <slav.donchev@gmail.com>
2019-09-25 09:48:09 +0300

    add wc address output

--- output.tf
+++ output.tf
@@ -5,3 +5,7 @@ output "webcounter_public_dns" {
 output "webcounter_public_ips" {
   value = module.webcounter.webcounter_public_ips
 }
+
+output "webcounter_addresses" {
+  value = [for dns in module.webcounter.webcounter_public_dns : "${dns}:8000/get"]
+}
